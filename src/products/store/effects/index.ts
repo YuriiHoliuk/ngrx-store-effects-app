3bf7269f61ade2259175bfe299c31d5c48a813c1
Yurii Holiuk <yuriiholiuk@gmail.com>
2017-12-10 14:51:51 +0200

    change imports

--- src/products/store/effects/index.ts
+++ src/products/store/effects/index.ts
@@ -1,5 +1,5 @@
-import * as fromPizzas from './pizzas.effect';
+import { PizzasEffects } from './pizzas.effect';
 
-export const effects = [fromPizzas.PizzasEffects];
+export const effects: any[] = [PizzasEffects];
 
 export * from './pizzas.effect';
--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -4,16 +4,17 @@ import { map, switchMap, catchError } from 'rxjs/operators'
 import { of } from 'rxjs/observable/of';
 
 import * as pizzasActions from '../actions/pizzas.action';
+
 import { Pizza } from '../../models/pizza.model';
 import { PizzasService } from './../../services/pizzas.service';
 
 export class PizzasEffects {
     constructor(private actions$: Actions, private pizzasService: PizzasService) {}
 
-    // @Effect()
-    // loadPizzas$ = this.actions$.ofType(pizzasActions.LOAD_PIZZAS).pipe(
-    //     switchMap(() => this.pizzasService.getPizzas()),
-    //     map((pizzas: Pizza[]) => new pizzasActions.LoadPizzasSuccess(pizzas)),
-    //     catchError((error: any) => of(new pizzasActions.LoadPizzasFail(error)))
-    // );
+    @Effect()
+    loadPizzas$ = this.actions$.ofType(pizzasActions.LOAD_PIZZAS).pipe(
+        switchMap(() => this.pizzasService.getPizzas()),
+        map((pizzas: Pizza[]) => new pizzasActions.LoadPizzasSuccess(pizzas)),
+        catchError((error: any) => of(new pizzasActions.LoadPizzasFail(error)))
+    );
 }
